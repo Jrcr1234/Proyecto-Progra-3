1dd09d229caab4293f4f72d4449cd749687fc5cb
Santiago Vega <S4ntivega@outlook.com>
2025-08-26 23:58:43 -0600

    ya aprendi a usar la vara

--- Hospital2/src/hospital/presentation/Medico/View.java
+++ Hospital2/src/hospital/presentation/Medico/View.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class View {
     private JPanel panel1;
     private JTextField textField1;
-    private JTextField textField2;
     private JTextField textField3;
     private JButton guardarButton;
     private JButton borrarButton;
