6991bc6a000ac4c51392de84087e59967c60c3cf
Rarthis <danielsanabriavelasquez@gmail.com>
2025-08-28 04:06:30 -0600

    view de medico

--- Hospital2/src/hospital/logic/Paciente.java
+++ Hospital2/src/hospital/logic/Paciente.java
@@ -1,4 +1,28 @@
 package hospital.logic;
 
 public class Paciente {
+
+    private String id;
+    private String nombre;
+    private int numeroTelefonico;
+
+
+    Paciente(String iD,String nombre,int numero){
+
+        this.id=iD;
+        this.nombre=nombre;
+        this.numeroTelefonico=numero;
+
+    }
+    Paciente(){
+        this.id=" ";
+        this.numeroTelefonico=0;
+        this.nombre=" ";
+
+    }
+    public String getId() {
+        return id;
+    }
+    public String getNombre(){return nombre;}
+    public int getNumeroTelefonico(){return numeroTelefonico;}
 }
--- Hospital2/src/hospital/presentation/admin/pacientes/View.java
+++ Hospital2/src/hospital/presentation/admin/pacientes/View.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 
 public class View {
     private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton guardarButton;
+    private JButton borrarButton;
+    private JTextField textField4;
+    private JButton buscarButton;
+    private JButton reporteButton;
 
     public View() {
         // El constructor está vacío por ahora.
